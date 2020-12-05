92f2f4773c339d47b15d33b94bcc1ecdada6bf6b
TxdoR <dubrulle.gaetan.59@gmail.com>
2020-12-05 13:34:13 +0100

    deploy menu

--- index.js
+++ index.js
@@ -22,15 +22,24 @@ closeMenu.addEventListener('click', () => {
 const services = document.getElementById('services')
 const section_drop_services = document.getElementById('section_drop_services')
 const section_header = document.getElementById('section_header')
+const close_menu_desktop = document.getElementById('close_menu_desktop');
+const dark_mode_menu_active = document.getElementById('dark_mode_menu_active')
 
 services.addEventListener('click', () => {
   setTimeout(() => {
-    section_drop_services.classList.add('active')
+    section_drop_services.classList.add('active');
+    dark_mode_menu_active.classList.add('active');
   },0)
 })
 
-document.body.addEventListener('click', () => {
-  section_drop_services.classList.remove('active')
+dark_mode_menu_active.addEventListener('click', () => {
+  section_drop_services.classList.remove('active');
+  dark_mode_menu_active.classList.remove('active');
+})
+
+close_menu_desktop.addEventListener('click', () => {
+  section_drop_services.classList.remove('active');
+  dark_mode_menu_active.classList.remove('active');
 })
 
 
@@ -55,3 +64,33 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+/**
+ * Form validation
+ */
+
+
+
+
+/**
+ * Scroll to section
+ */
+
+
+
+
+/**
+ * Potentiel entreprise
+ * 
+ * [] Secteur d'activité
+ * [] dropwdon metier
+ * [] Types de clients
+ * [] Localisation (france ou belgique)
+ * [] As deja un site
+ * [] Si oui : est-ce qu'il est mis a jour / responsive 
+ * [] etes vous present sur les reseaux sociaux
+ * [] resultats fictifs
+ * [] bar de progression
+ */
+
